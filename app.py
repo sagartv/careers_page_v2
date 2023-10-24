b4087d2ef40f1fcfd55d58f88baa1797311c4f4a
sagartv <31515867+sagartv@users.noreply.github.com>
2023-10-24 17:32:12 +0000

    Added comments to app.py and database.py

--- app.py
+++ app.py
@@ -3,14 +3,23 @@ from database import get_jobs_from_db
 
 app = Flask(__name__)
 
+#route to this function when nothing succeeds the "/" in the url
 @app.route('/')
 def hello_careers():
-    jobs = get_jobs_from_db()
-    return render_template('home.html', jobs = jobs)
   
+  #Get the Jobs from the MySQL database hosted in the cloud
+  jobs = get_jobs_from_db()
+  #Render the home.html page
+  return render_template('home.html', jobs = jobs)
+
+
+#route to this function when "/api/jobs" is in the url
 @app.route('/api/jobs')
 def list_jobs():
+  
+  #Get the Jobs from the MySQL database hosted in the cloud
   jobs = get_jobs_from_db()
+  #return a JSON with the jobs list
   return jsonify(jobs)
   
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -3,7 +3,10 @@ import sqlalchemy
 from sqlalchemy import create_engine, text
 import os
 
+#Get the Database string, username, hostname and password from the environment variables/secrets
 db_conn_string = os.environ['DB_CONNECTION_STRING']
+
+#create a connection, passing arguments with SSL certification for a secure connection
 
 engine = create_engine(db_conn_string, pool_pre_ping=True, connect_args= {"ssl":{"ssl_ca":"etc/ssl/cert.pem"}})
 
@@ -11,8 +14,10 @@ def get_jobs_from_db():
 
   with engine.connect() as conn:
     jobs = []
+    #query the Cloud-hosted MySQL Database for the jobs
     result = conn.execute(text('SELECT * FROM jobs'))
     for row in result.all():
+# Get the row, change it to dict by invoking _asdict(), add to list of jobs
       jobs.append(row._asdict())
     return jobs
 
