f1109e92c8680942d72b0af01a7543f9ce671985
sagartv <31515867+sagartv@users.noreply.github.com>
2023-10-26 10:53:07 +0000

    Added jobpage.html

--- app.py
+++ app.py
@@ -25,7 +25,9 @@ def list_jobs():
 @app.route('/api/job/<id>')
 def show_job(id):
   job = get_job_from_db(id)
-  return jsonify(job)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job = job)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=81, debug = True)
