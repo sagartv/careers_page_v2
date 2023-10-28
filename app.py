e03c858816cdfdffbd34a30b0f827acd3c465c17
sagartv <31515867+sagartv@users.noreply.github.com>
2023-10-28 14:47:27 +0000

    Added Application Form to JobPage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import get_jobs_from_db, get_job_from_db
 
 app = Flask(__name__)
@@ -28,6 +28,16 @@ def show_job(id):
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html', job = job)
+
+
+@app.route('/job/<id>/apply', methods= ['post'])
+def apply_job(id):
+  data = request.form
+  return jsonify(data)
+
+
+
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=81, debug = True)
